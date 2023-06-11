b3203b452b6dd115eba646d94e51393342257b66
Ulisses <ulissessuporteti01@gmail.com>
2023-06-11 11:47:25 -0300

    feat: add all initial files

---
+++ src/App/index.tsx
@@ -0,0 +1,12 @@
+import * as C from './styles';
+
+function App() {
+
+	return (
+		<C.App>   
+			<h1 >Hello World</h1>
+		</C.App>
+	);
+}
+
+export default App;
---
+++ src/App/styles.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+
+export const App = styled.main`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  padding: 32px;
+  width: 100%;
+  
+  h1 {
+    text-align: center;
+  }
+`;
