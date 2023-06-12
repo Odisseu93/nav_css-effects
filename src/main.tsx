5daff350baad52ceaec77dded68b3e1a21531bb3
Ulisses <ulissessuporteti01@gmail.com>
2023-06-12 18:06:50 -0300

    fix: add page title and favicon and arrange main.tsx import

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,9 @@
+import * as C from './styles/globals.ts';
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
 import App from './App/index.tsx';
-import * as C from './styles/globals.ts';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
